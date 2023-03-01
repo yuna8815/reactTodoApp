f6c49f0c4a60602e4969915ca8f35739485bd187
KimYuNa <hugyaa@gmail.com>
2023-03-01 17:18:25 +0900

    fix: 입력 부분 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
 import List from './components/List';
 
 export default function App() {
@@ -14,11 +15,6 @@ export default function App() {
     completed: false
   }])
   const [value, setValue] = useState("")
-
-  const handleChange = (e) => {
-    console.log('e', e.target.value);
-    setValue(e.target.value)
-  }
 
   const handleSubmit = (e) => {
     // 페이지의 리로드를 방지
@@ -42,23 +38,7 @@ export default function App() {
         </div>
 
         <List todoData={todoData} setTodoData={setTodoData} />
-
-        <form style={{ display: 'flex'}} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{flex: '10', padding:'5px' }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: 1 }}
-          />
-        </form>
+        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
       </div>
     </div>
   )
---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Form({ value, setValue, handleSubmit}) {
+  const handleChange = (e) => {
+    console.log('e', e.target.value);
+    setValue(e.target.value)
+  }
+  return (
+    <form style={{ display: 'flex'}} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="value"
+        style={{flex: '10', padding:'5px' }}
+        placeholder="해야 할 일을 입력하세요."
+        value={value}
+        onChange={handleChange}
+      />
+      <input
+        type="submit"
+        value="입력"
+        className="btn"
+        style={{ flex: 1 }}
+      />
+    </form>
+  )
+}
