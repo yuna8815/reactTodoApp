d08c7b8cccb2da286471db64a09a64ab3873e53b
KimYuNa <hugyaa@gmail.com>
2023-03-01 14:40:56 +0900

    feat: 목록 UI 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import React, {Component} from 'react';
 import './App.css';
 
 export default class App extends Component {
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+  }
+
+  // 동적 스타일링을 위해 함수형 스타일선언
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
   render() {
     return(
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할일 목록</h1>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button style={this.btnStyle}>X</button>
           </div>
         </div>
       </div>
